a636d3da20b748ee50ad061a4e64f0213ec8c149
lzl2040 <2040171586@qq.com>
2025-09-09 16:37:41 +0000

    update

--- lerobot/common/policies/uni_tk/configuration_uni_tk.py
+++ lerobot/common/policies/uni_tk/configuration_uni_tk.py
@@ -17,7 +17,8 @@ class UniTKConfig(PreTrainedConfig):
     # qwen_path: str = "/Data/lzl/qwen2.5_vl_7b/Qwen2.5-VL-7B-Instruct"
     # vlm_path: str = "/home/v-zuoleili/Pretrain/InternVL3_5-1B-HF"
     # img_pred_model: str = "stabilityai/stable-diffusion-3.5-medium"
-    vlm_path: str = "/mnt/v-wangxiaofa/pt_weights/InternVL3_5-1B-HF"
+    # vlm_path: str = "/mnt/v-wangxiaofa/pt_weights/InternVL3_5-1B-HF"
+    vlm_path: str = "OpenGVLab/InternVL3_5-1B-HF"
     img_pred_model: str = "stabilityai/stable-diffusion-3.5-medium"
     
     # Input / output structure.
