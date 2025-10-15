7e53fc9d9208723246f6fbef9c44db02b8a4eacd
lzl2040 <2040171586@qq.com>
2025-10-15 02:38:14 +0000

    update sh

--- train_uni_token.sh
+++ train_uni_token.sh
@@ -23,6 +23,7 @@ LOSS_TYPE="raw"
 IS_FT=true
 IP_TOKEN_NUM=16
 IP_TOKEN_GEN_TYPE="cls_proj"cls_proj
+IMG_DECODER_PART_TRAIN=true
 
 # 解析命令行参数
 while [[ $# -gt 0 ]]; do
@@ -99,6 +100,10 @@ while [[ $# -gt 0 ]]; do
             IP_TOKEN_GEN_TYPE="$2"
             shift 2
             ;;
+        --img_decoder_part_train)
+            IMG_DECODER_PART_TRAIN="$2"
+            shift 2
+            ;;
         --calvin_sub_task)
             CALVIN_SUB_TASK="$2"
             shift 2
@@ -158,6 +163,7 @@ CUDA_LAUNCH_BLOCKING=1 torchrun \
     --policy.loss_type=$LOSS_TYPE \
     --policy.ip_token_num=$IP_TOKEN_NUM \
     --policy.ip_token_gen_type=$IP_TOKEN_GEN_TYPE \
+    --policy.img_decoder_part_train=$IMG_DECODER_PART_TRAIN \
     --policy.chunk_size=$((MAX_FRAME-1)) \
     --policy.n_action_steps=$((MAX_FRAME-1)) \
     --output_dir="$FIXED_OUTPUT_DIR" \
