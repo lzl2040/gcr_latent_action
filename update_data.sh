0ec9b30fef956e380dead3c934fa124b05e50952
lzl2040 <2040171586@qq.com>
2025-10-27 13:06:22 +0000

    update sh

---
+++ update_data.sh
@@ -0,0 +1,12 @@
+DATA_MIX="simpler_bridge"
+# 解析命令行参数
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --data_mix)
+            USE_STATE="$2"
+            shift 2
+            ;;
+    esac
+done
+python lerobot/common/datasets/v21/convert_dataset_v20_to_v21.py \
+    --data_mix $DATA_MIX
