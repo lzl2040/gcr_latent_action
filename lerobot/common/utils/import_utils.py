57b11aaafab00ff7d8ce3b7a4ef1d9241b130535
lzl2040 <2040171586@qq.com>
2025-10-28 13:48:13 +0000

    update

--- lerobot/common/utils/import_utils.py
+++ lerobot/common/utils/import_utils.py
@@ -57,3 +57,5 @@ _torch_available, _torch_version = is_package_available("torch", return_version=
 _gym_xarm_available = is_package_available("gym_xarm")
 _gym_aloha_available = is_package_available("gym_aloha")
 _gym_pusht_available = is_package_available("gym_pusht")
+
+_transformers_available = is_package_available("transformers")
