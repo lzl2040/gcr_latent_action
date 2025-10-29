49d7879eb6fb3743d4339ae932c1981f65f72793
lzl2040 <2040171586@qq.com>
2025-10-29 07:39:13 +0000

    update mixtures

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -139,6 +139,36 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         ("ego_dex", 0.02),
     ],
 
+    "oxe_magic_soup_plus_minus_v1": [
+        ("berkeley_cable_routing", 1.0),
+        ("fractal20220817_data", 0.54087122203),                # Google RT-1 Robot Data (Large-Scale)
+        ("kuka", 0.8341046294),
+        ("bridge_orig", 1.0),                                   # Original Version of Bridge V2 from Project Website
+        ("taco_play", 2.0),
+        ("jaco_play", 1.0),
+        ("roboturk", 2.0),
+        ("viola", 2.0),
+        ("berkeley_autolab_ur5", 2.0),
+        ("toto", 1.0),
+        ("language_table", 0.1),
+        ("stanford_hydra_dataset_converted_externally_to_rlds", 2.0),
+        ("austin_buds_dataset_converted_externally_to_rlds", 1.0),
+        ("nyu_franka_play_dataset_converted_externally_to_rlds", 3.0),
+        ("furniture_bench_dataset_converted_externally_to_rlds", 0.1),
+        ("ucsd_kitchen_dataset_converted_externally_to_rlds", 2.0),
+        ("austin_sailor_dataset_converted_externally_to_rlds", 1.0),
+        ("austin_sirius_dataset_converted_externally_to_rlds", 1.0),
+        ("dlr_edan_shared_control_converted_externally_to_rlds", 1.0),
+        ("iamlab_cmu_pickup_insert_converted_externally_to_rlds", 1.0),
+        ("utaustin_mutex", 1.0),
+        ("berkeley_fanuc_manipulation", 2.0),
+        ("cmu_stretch", 1.0),
+        ## New Datasets in MagicSoup++
+        ("bc_z", 0.2),                                          # Note: use v0.1.0 --> later versions broken
+        ("fmb_dataset", 1.0),
+        ("dobbe", 0.2),
+    ],
+
     "oxe_magic_soup_plus_minus": [
         ("fractal20220817_data", 1.0),                          # Google RT-1 Robot Data (Large-Scale)
         ("kuka", 0.8341046294),
@@ -195,6 +225,11 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         ("droid_wipe", 1.0),
     ],
 
+    "debug_data": [
+        ("stanford_hydra_dataset_converted_externally_to_rlds", 2.0),
+        ("austin_buds_dataset_converted_externally_to_rlds", 1.0),
+    ],
+
     # === LIBERO Datasets (Modified Versions) ===
     "libero_spatial_no_noops": [
         ("libero_spatial_no_noops", 1.0),
@@ -246,6 +281,9 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
      ],
      "libero_10": [
          ("libero_10_no_noops", 1.0), 
+     ],
+     "libero_object": [
+         ("libero_object_no_noops", 1.0), 
      ],
      "simpler_fractal":[
          ("simpler_fractal", 1.0), 
