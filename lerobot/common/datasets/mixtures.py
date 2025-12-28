6e036b7f25b3275d3a835dd5d2d1801d0e522025
lzlglory <lizuolei24@ime.ac.cn>
2025-12-28 14:24:28 +0000

    update

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -137,7 +137,6 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         ("droid", 0.001),
         ("agibot_alpha", 0.1),
         ("ego_dex_split5", 0.08),
-        ("ego_dex_split2", 0.08),
         # ("ego_dex_split3", 0.08),
         ("game_minecraft", 1.0),
         ("game_stardew", 1.0),
