9a42701493566f6ea0c241b7af84f727a69fbc61
lzl2040 <2040171586@qq.com>
2026-04-10 15:37:12 +0000

    update mixture

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -279,7 +279,7 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         ("bc_z", 0.2),                                          # Note: use v0.1.0 --> later versions broken
         ("fmb_dataset", 1.0),
         ("dobbe", 0.2),
-        ("droid", 0.1),
+        ("droid", 0.01),
         ("agibot_alpha", 0.1),
         ("robomind_franka_dual_arm", 0.1),
         ("robomind_franka_3rgb", 0.5),
@@ -291,7 +291,7 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         # ("interna1_dual_arm_2", 0.05),
         # ("interna1_dual_arm_3", 0.05),
         # ("interna1_dual_arm_4", 0.1),
-        # ("interna1_single_arm", 0.05),
+        ("interna1_single_arm", 0.05),
     ],
     
     "debug":[
