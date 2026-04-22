cfec77ac5b0bf4b5a991084d32fb2a6275a36ce8
lzl2040 <2040171586@qq.com>
2026-04-22 12:51:18 +0000

    update add interna1

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -334,7 +334,7 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
         # ("interna1_dual_arm_2", 0.05),
         # ("interna1_dual_arm_3", 0.05),
         # ("interna1_dual_arm_4", 0.1),
-        # ("interna1_single_arm", 0.1),
+        ("interna1_single_arm", 0.05),
     ],
     
     "ms_complete_data_wo_sim":[
