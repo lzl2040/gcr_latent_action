bcc8405cfad1ce0ac8365fd5f7367a0ffcb4f368
lzl2040 <2040171586@qq.com>
2026-07-03 15:06:00 +0000

    update debug

--- lerobot/common/datasets/mixtures.py
+++ lerobot/common/datasets/mixtures.py
@@ -347,10 +347,10 @@ OXE_NAMED_MIXTURES: Dict[str, List[Tuple[str, float]]] = {
     "ms_buy_v30":[
         ("ms_data_scale", 1.0),
         ("ms_data_xdof_1", 0.15),
-        ("ms_data_xdof_2", 0.15),
-        ("ms_data_xdof_3", 0.15),
+        # ("ms_data_xdof_2", 0.15),
+        # ("ms_data_xdof_3", 0.15),
         ("ms_data_xdof_4", 0.15),
-        ("ms_data_xdof_5", 0.15),
+        # ("ms_data_xdof_5", 0.15),
     ],
     
     "ms_complete_data_wo_sim":[
